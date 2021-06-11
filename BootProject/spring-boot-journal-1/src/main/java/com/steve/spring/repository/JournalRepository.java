a692e404d5c07f23025d92b84368026f95f58f29
stevePark <38517823+201724468@users.noreply.github.com>
2021-06-11 17:24:22 +0900

    Delete BootProject directory

--- BootProject/spring-boot-journal-1/src/main/java/com/steve/spring/repository/JournalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.steve.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.steve.spring.domain.Journal;
-
-public interface JournalRepository extends JpaRepository<Journal, Long>{
-
-}
